d54d65ba8c3796d8ce50a6f616cd72240be34bcc
YangYi <all381en@gmail.com>
2015-03-09 09:49:41 -0700

    Finish lab 1

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,35 +9,27 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
+	
 	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
+	mov r4,#0
+	mov r3,#1
+	sub r5,r0,#1
+	cmp r0,#1
+	beq .L4	
+.L2:
+	cmp r5,#0
+	beq .L3 
+	sub r5,r5,1
+	add r0,r3,r4
+	mov r4,r3	
+	mov r3,r0
+	b .L2
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	pop {r3, r4, r5, pc}		@EPILOG
 .L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	mov r0,#1
+	pop {r3, r4, r5, pc}		@EPILOG
+	@ END CODE MODIFICATION
 
 	.size fibonacci, .-fibonacci
 	.end
